e82f0c1059ea58059fae9a3fc26e67bbc71dc147
戴　岩沢 <daiyanze@EverySense-Japan-no-MacBook-Air.local>
2017-05-09 18:07:13 +0900

    implementing travis ci

--- setup.py
+++ setup.py
@@ -6,17 +6,14 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='everyclient',
-    version='0.1.0',
+    version='0.1.2',
     description='Everysense client package for Rest API',
     long_description=readme,
     author='Everysense Inc',
     author_email='support@every-sense.com',
     url='https://github.com/every-sense',
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests', 'example', 'docs'))
 )
