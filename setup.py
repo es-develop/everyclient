862c39d72f7ac287cac2ce4253a4097f1c1ea0c6
daiyanze <ginoalex8964@yahoo.com>
2017-05-16 13:41:42 +0900

    v0.1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='everyclient',
-    version='0.1.5',
+    version='0.1.6',
     description='Everysense client package for Rest API',
     long_description=readme,
     author='Everysense Inc',
